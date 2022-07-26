af73c13dfbe0d2fe89e3a55332d35f2499a7b86d
GitDemo <wjsalswo985@naver.com>
2022-07-26 11:27:56 +0900

    추가

--- src/Studentinfo.java
+++ src/Studentinfo.java
@@ -1,3 +1,11 @@
+
+/*
+ * 과제시 추가해야할 주석
+ * 누가
+ * 언제
+ * 만든 목적
+ * 작성환경(OS : Mac Os, JDK Version : OpenJdk 11.0.16, Editor : Eclipse 2022-06)
+*/
 import java.util.Scanner;
 
 public class Studentinfo {
@@ -17,5 +25,6 @@ public class Studentinfo {
 		System.out.println("이름 :"+name);
 		System.out.println("학번 :"+hakbun);
 		System.out.println("주소 :"+adress);
+		
 	}
 }
